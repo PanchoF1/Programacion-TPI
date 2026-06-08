4b178256f149e713d6ad6dfef75a89f74e8df851
Urielarey <uro2006arey@gmail.com>
2026-06-08 14:40:15 -0300

    agrego funcion buscarPais

--- datos.py
+++ datos.py
@@ -96,3 +96,25 @@ def actualizarPais (paises):
             return
     except Exception as e:
         print(e)
+
+def buscarPais(paises):
+    buscarPais = input("Ingrese el nombre del pais a consultar: ")
+    buscarPais = buscarPais.title()
+    buscarPais = buscarPais.strip()
+    if buscarPais == "":
+        print("El nombre del pais no puede estar vacio")
+        return
+    elif len(paises) == 0:
+        print("Archivo vacio")
+        return
+    encontrado = False
+    for pais in paises:
+        if pais["pais"].title().strip() == buscarPais: 
+            print(f"""Los datos de {buscarPais} son: 
+                Poblacion: {pais["poblacion"]} 
+                Superficie: {pais["superficie"]} 
+                Continente: {pais["continente"]} """)
+            encontrado = True
+    if not encontrado:
+        print("Pais no encontrado")
+    return
