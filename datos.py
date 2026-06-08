365a0381cb5164efb7f348248961b5e5757e457e
Urielarey <uro2006arey@gmail.com>
2026-06-08 15:00:10 -0300

    arreglo errores

--- datos.py
+++ datos.py
@@ -109,7 +109,7 @@ def buscarPais(paises):
         return
     encontrado = False
     for pais in paises:
-        if pais["pais"].title().strip() == buscarPais: 
+        if pais["nombre"].title().strip() == buscarPais: 
             print(f"""Los datos de {buscarPais} son: 
                 Poblacion: {pais["poblacion"]} 
                 Superficie: {pais["superficie"]} 
